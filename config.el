13d6b284582dcf276c940eb9799a85c5e53abf8e
Paul Lemmens <paul.lemmens@philips.com>
2016-10-27 08:50:32 +0200

    Cleaned up the code by setting all keys in one statement (picked this up somewhere else).

--- config.el
+++ config.el
@@ -1,31 +1,34 @@
-(spacemacs/set-leader-keys "oo"  'todotxt)      ; Open todo.txt
-(spacemacs/set-leader-keys "otl"  'todotxt-unhide-all)      ; (L)ist
-(spacemacs/set-leader-keys "oti"  'todotxt-show-incomplete) ; list (I)ncomplete
-(spacemacs/set-leader-keys "otx"  'todotxt-complete-toggle) ; (C)omplete item
-;; (spacemacs/set-leader-keys "ox"  'todotxt-complete-toggle) ; (C)omplete item
-(spacemacs/set-leader-keys "otN"  'todotxt-nuke-item)       ; (N)uke item
-(spacemacs/set-leader-keys "ota"  'todotxt-add-item-any-buffer)        ; (A)dd item
-;; (spacemacs/set-leader-keys "oa"   'todotxt-add-item-any-buffer)        ; (A)dd item in todo from any buffer.
-(spacemacs/set-leader-keys "otq"  'todotxt-bury)            ; (Q)uit
-;; (spacemacs/set-leader-keys "oq"  'todotxt-bury)            ; (Q)uit
-(spacemacs/set-leader-keys "otr"  'todotxt-add-priority)    ; Add p(r)iority
-(spacemacs/set-leader-keys "otp"  'todotxt-add-priority)    ; Add p(r)iority
-(spacemacs/set-leader-keys "otA"  'todotxt-archive)         ; (A)rchive completed items
-(spacemacs/set-leader-keys "ote"  'todotxt-edit-item)       ; (E)dit item
-(spacemacs/set-leader-keys "oe"  'todotxt-edit-item)       ; (E)dit item
-(spacemacs/set-leader-keys "ott"  'todotxt-tag-item)        ; (T)ag item
-(spacemacs/set-leader-keys "otd"  'todotxt-add-due-date)    ; (D)ue date
-(spacemacs/set-leader-keys "ot/"  'todotxt-filter-for)      ;
-;; (spacemacs/set-leader-keys "o/"  'todotxt-filter-for)      ;
-(spacemacs/set-leader-keys "ot\\" 'todotxt-filter-out)      ;
-(spacemacs/set-leader-keys "otg"  'todotxt-revert)          ; Revert the buffer
-(spacemacs/set-leader-keys "ots"  'save-buffer)             ; (S)ave
-(spacemacs/set-leader-keys "otu"  'todotxt-undo)            ; (U)ndo
-;; (spacemacs/set-leader-keys "n"    'next-line)               ; (N)ext
-;; (spacemacs/set-leader-keys "p"    'previous-line)           ; (P)revious
-;;
-;; Something in the following three lines, spacemacs does not like. Gives
-;; error on both the j and the oth/ot? command. Keeping for future references.
-;; (spacemacs/set-leader-keys "j"    'next-line)               ; Vi Binding
-;; (spacemacs/set-leader-keys "k"    'previous-line)           ; Vi Binding
-;; (spacemacs/set-leader-keys "ot?"  'describe-mode)           ; Help!
+(spacemacs/set-leader-keys
+  ;; "oo"   'todotxt      ; Open todo.txt
+  "oto"  'todotxt      ; Open todo.txt
+  "otl"  'todotxt-unhide-all      ; (L)ist
+  "oti"  'todotxt-show-incomplete ; list (I)ncomplete
+  "otx"  'todotxt-complete-toggle ; (C)omplete item
+  ;; (spacemacs/set-leader-keys "ox"  'todotxt-complete-toggle ; (C)omplete item
+  "otN"  'todotxt-nuke-item       ; (N)uke item
+  "ota"  'todotxt-add-item-any-buffer        ; (A)dd item
+  ;; "oa"   'todotxt-add-item-any-buffer        ; (A)dd item in todo from any buffer.
+  "otq"  'todotxt-bury            ; (Q)uit
+  ;; "oq"  'todotxt-bury            ; (Q)uit
+  "otr"  'todotxt-add-priority    ; Add p(r)iority
+  "otp"  'todotxt-add-priority    ; Add p(r)iority
+  "otA"  'todotxt-archive         ; (A)rchive completed items
+  "ote"  'todotxt-edit-item       ; (E)dit item
+  ;; "oe"  'todotxt-edit-item       ; (E)dit item
+  "ott"  'todotxt-tag-item        ; (T)ag item
+  "otd"  'todotxt-add-due-date    ; (D)ue date
+  "ot/"  'todotxt-filter-for      ;
+  ;; "o/"  'todotxt-filter-for      ;
+  "ot\\" 'todotxt-filter-out      ;
+  "otg"  'todotxt-revert          ; Revert the buffer
+  "ots"  'save-buffer             ; (S)ave
+  "otu"  'todotxt-undo            ; (U)ndo
+  ;; "n"    'next-line               ; (N)ext
+  ;; "p"    'previous-line           ; (P)revious
+  ;;
+  ;; Something in the following three lines, spacemacs does not like. Gives
+  ;; error on both the j and the oth/ot? command. Keeping for future references.
+  ;; "j"    'next-line               ; Vi Binding
+  ;; "k"    'previous-line           ; Vi Binding
+  ;; "ot?"  'describe-mode           ; Help!
+  )
